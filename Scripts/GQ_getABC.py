dfd4c59afd86b213b15d013420f005f6f979018f
gquelch <64699318+gquelch@users.noreply.github.com>
2020-06-21 13:39:18 +0100

    remove run() func

--- Scripts/GQ_getABC.py
+++ Scripts/GQ_getABC.py
@@ -1,149 +1,148 @@
 import os
 import nuke
 
-def run():
-	def GQ_getABC():
-		nuke.root().begin()
-		
-		abcList = []
-		geoLoop = 0
-		replaceCam = False
-		
 
-		mySel = nuke.root().selectedNodes()
-		
-		alembicPath = GQ_getABCPath()
+def GQ_getABC():
+    nuke.root().begin()
+    
+    abcList = []
+    geoLoop = 0
+    replaceCam = False
+    
 
-		#get folders and contents
-		for path, subdirs, files in os.walk(alembicPath):
-			for file in files:
-				if file.endswith(".abc"):
-					abcList.append(file)
+    mySel = nuke.root().selectedNodes()
+    
+    alembicPath = GQ_getABCPath()
 
-
-		#UI for selecting alembics
-		p = nuke.Panel("GQ_getABC")
-		for i in abcList:
-			p.addBooleanCheckBox(i,False)
-
-		if not p.show():
-			return
+    #get folders and contents
+    for path, subdirs, files in os.walk(alembicPath):
+        for file in files:
+            if file.endswith(".abc"):
+                abcList.append(file)
 
 
+    #UI for selecting alembics
+    p = nuke.Panel("GQ_getABC")
+    for i in abcList:
+        p.addBooleanCheckBox(i,False)
 
-		#selection Dict
-		ABCDict = {}
-
-		for i in abcList:
-			ABCDict[i] = p.value(i)
-
-		#check for ABCs marked as True, and return as 2 lists (Cam + Geo)
-		importABCLists = GQ_ABCTypeCheck(ABCDict)
-
-		camList = importABCLists[0]
-		geoList = importABCLists[1]
-	 
-
-		if len(camList) == 1:
-			if len(mySel) == 1:
-				if mySel[0].Class() == "Camera2":
-					p2 = nuke.Panel("overWrite_Check")
-
-					p2.addBooleanCheckBox("replace selected camera?",False)
-
-					if not p2.show():
-						return
-
-					replaceCam = p2.value("replace selected camera?")    
-		
-		for cam in camList:
-			camPath = alembicPath + "/" + cam
-
-			#repalce selected camera
-			if replaceCam == True:    
-				mySel[0].knob("file").setValue(camPath)
-				mySel[0].knob("frame_rate").setValue(nuke.root()["fps"].getValue())        
-
-			#creating new cameras
-			if replaceCam == False:
-				c = nuke.nodes.Camera2()
-
-				c.knob("read_from_file").setValue(True)
-				c.knob("file").setValue(camPath)
-				c.knob("frame_rate").setValue(nuke.root()["fps"].getValue())
-
-				if len(mySel) > 0:
-					c.knob("xpos").setValue(mySel[0].knob("xpos").getValue()-150)
-					c.knob("ypos").setValue(mySel[0].knob("ypos").getValue()-25)
-
-		for geo in geoList:
-			geoPath = alembicPath + "/" + geo
-
-			rg = nuke.nodes.ReadGeo2()
-
-			rg.knob("file").setValue(geoPath)
-			rg.knob("frame_rate").setValue(nuke.root()["fps"].getValue())
-
-			if len(mySel) > 0:
-				if geoLoop == 0:
-					rg.knob("xpos").setValue(mySel[0].knob("xpos").getValue()-250)
-					rg.knob("ypos").setValue(mySel[0].knob("ypos").getValue()-25)
-				elif geoLoop > 0:
-					rg.knob("xpos").setValue(lastRg.knob("xpos").getValue()-100)
-					rg.knob("ypos").setValue(lastRg.knob("ypos").getValue())
-
-				geoLoop += 1
-				lastRg = rg
-
-	def GQ_getABCPath():
-		## Get ABC Path ##
-
-		#file path
-		path = nuke.root().knob('name').value()
-
-		'''
-		This is where you want to  get the path to your alembics
-		This will be different for different studios
-		Some places will alembic to a different drive, some to a "publish" version of that shot
-		You should be able to retrieve this information from either the shot name itself
-		or what i opt for is from the full file path, this usually contains the information, 
-		and means you dont need to worry about file naming
-		
-		You want to return the full path to the folder containing your alembics
-
-		'''
+    if not p.show():
+        return
 
 
-		#abcPath =
 
-		return abcPath
+    #selection Dict
+    ABCDict = {}
 
-	def GQ_ABCTypeCheck(ABCDict):
-		## Split ABC Types ##
+    for i in abcList:
+        ABCDict[i] = p.value(i)
 
-		geoList = []
-		camList = []
+    #check for ABCs marked as True, and return as 2 lists (Cam + Geo)
+    importABCLists = GQ_ABCTypeCheck(ABCDict)
 
-		for key in ABCDict:
-			if ABCDict[key] == True:
+    camList = importABCLists[0]
+    geoList = importABCLists[1]
+ 
 
-				'''
-				Here you want to seperate out 2 types of alembics: Geometry and Cameras
-				They both require different nodes to be created in Nuke, meaning we need to define the difference
-				I have left an example below where I am checking the file name for either "Rigging" or "Camera"
-				
-				You may cache things based on obj type, e.g CAM, PRP, CHR, VHL, in which case you could use this to define
-				
-				if "CAM" in key:
-					camList.append(key)
+    if len(camList) == 1:
+        if len(mySel) == 1:
+            if mySel[0].Class() == "Camera2":
+                p2 = nuke.Panel("overWrite_Check")
 
-				if "GEO" in key:
-					geoList.append(key)
-				'''
+                p2.addBooleanCheckBox("replace selected camera?",False)
 
-		return camList, geoList
-		
-	   
-	GQ_getABC()
+                if not p2.show():
+                    return
 
-run()
+                replaceCam = p2.value("replace selected camera?")    
+    
+    for cam in camList:
+        camPath = alembicPath + "/" + cam
+
+        #repalce selected camera
+        if replaceCam == True:    
+            mySel[0].knob("file").setValue(camPath)
+            mySel[0].knob("frame_rate").setValue(nuke.root()["fps"].getValue())        
+
+        #creating new cameras
+        if replaceCam == False:
+            c = nuke.nodes.Camera2()
+
+            c.knob("read_from_file").setValue(True)
+            c.knob("file").setValue(camPath)
+            c.knob("frame_rate").setValue(nuke.root()["fps"].getValue())
+
+            if len(mySel) > 0:
+                c.knob("xpos").setValue(mySel[0].knob("xpos").getValue()-150)
+                c.knob("ypos").setValue(mySel[0].knob("ypos").getValue()-25)
+
+    for geo in geoList:
+        geoPath = alembicPath + "/" + geo
+
+        rg = nuke.nodes.ReadGeo2()
+
+        rg.knob("file").setValue(geoPath)
+        rg.knob("frame_rate").setValue(nuke.root()["fps"].getValue())
+
+        if len(mySel) > 0:
+            if geoLoop == 0:
+                rg.knob("xpos").setValue(mySel[0].knob("xpos").getValue()-250)
+                rg.knob("ypos").setValue(mySel[0].knob("ypos").getValue()-25)
+            elif geoLoop > 0:
+                rg.knob("xpos").setValue(lastRg.knob("xpos").getValue()-100)
+                rg.knob("ypos").setValue(lastRg.knob("ypos").getValue())
+
+            geoLoop += 1
+            lastRg = rg
+
+def GQ_getABCPath():
+    ## Get ABC Path ##
+
+    #file path
+    path = nuke.root().knob('name').value()
+
+    '''
+    This is where you want to  get the path to your alembics
+    This will be different for different studios
+    Some places will alembic to a different drive, some to a "publish" version of that shot
+    You should be able to retrieve this information from either the shot name itself
+    or what i opt for is from the full file path, this usually contains the information, 
+    and means you dont need to worry about file naming
+    
+    You want to return the full path to the folder containing your alembics
+
+    '''
+
+
+    abcPath =
+
+    return abcPath
+
+def GQ_ABCTypeCheck(ABCDict):
+    ## Split ABC Types ##
+
+    geoList = []
+    camList = []
+
+    for key in ABCDict:
+        if ABCDict[key] == True:
+
+            '''
+            Here you want to seperate out 2 types of alembics: Geometry and Cameras
+            They both require different nodes to be created in Nuke, meaning we need to define the difference
+            I have left an example below where I am checking the file name for either "Rigging" or "Camera"
+            
+            You may cache things based on obj type, e.g CAM, PRP, CHR, VHL, in which case you could use this to define
+            
+            if "CAM" in key:
+                camList.append(key)
+
+            if "GEO" in key:
+                geoList.append(key)
+            '''
+
+    return camList, geoList
+    
+   
+GQ_getABC()
+
--- Scripts/GQ_gradeConvert.py
+++ Scripts/GQ_gradeConvert.py
@@ -1,64 +1,64 @@
+import os
 import nuke
+import nukescripts
 
-def run():
-	def gq_gradeConvertWin():
 
-		p = nuke.Panel("GQ_gradeConvert")
+def gq_gradeConvertWin():
 
-		p.addEnumerationPulldown("Tone","Master Shadows Midtones Highlights") #get user input for colour correct target
+    p = nuke.Panel("GQ_gradeConvert")
 
-		if not p.show():
-			return
+    p.addEnumerationPulldown("Tone","Master Shadows Midtones Highlights") #get user input for colour correct target
 
-		target = p.value("Tone") #set user input to "target"
+    if not p.show():
+        return
 
-		gq_gradeConvert(target)
+    target = p.value("Tone") #set user input to "target"
 
-	def gq_gradeConvert(target):
-		if target == "Master":
-			gain = "gain"
-			gamma = "gamma"
-			add = "offset"
-		if target == "Shadows":
-			gain = "shadows.gain"
-			gamma = "shadows.gamma"
-			add = "shadows.offset"
-		if target == "Midtones":
-			gain = "midtones.gain"
-			gamma = "midtones.gamma"   
-			add = "midtones.offset"    
-		if target == "Highlights":
-			gain = "highlights.gain"
-			gamma = "highlights.gamma"  
-			add = "highlights.offset" 
+    gq_gradeConvert(target)
 
-		mySel = nuke.root().selectedNodes()
-		
+def gq_gradeConvert(target):
+    if target == "Master":
+        gain = "gain"
+        gamma = "gamma"
+        add = "offset"
+    if target == "Shadows":
+        gain = "shadows.gain"
+        gamma = "shadows.gamma"
+        add = "shadows.offset"
+    if target == "Midtones":
+        gain = "midtones.gain"
+        gamma = "midtones.gamma"   
+        add = "midtones.offset"    
+    if target == "Highlights":
+        gain = "highlights.gain"
+        gamma = "highlights.gamma"  
+        add = "highlights.offset" 
 
-		for i in mySel:
-			if i.Class() == "Grade":
-				c = nuke.nodes.ColorCorrect()
+    mySel = nuke.root().selectedNodes()
+    
 
-				#colour inputs
-				c.knob(gain).setValue(i.knob("white").getValue())
-				c.knob(gamma).setValue(i.knob("gamma").getValue())
-				c.knob(add).setValue(i.knob("add").getValue())
+    for i in mySel:
+        if i.Class() == "Grade":
+            c = nuke.nodes.ColorCorrect()
 
-				#mix and mask inputs
-				c.knob("mix").setValue(i.knob("mix").getValue())
-				iMaskInput = i.input(1)
-				iColInput = i.input(0)
+            #colour inputs
+            c.knob(gain).setValue(i.knob("white").getValue())
+            c.knob(gamma).setValue(i.knob("gamma").getValue())
+            c.knob(add).setValue(i.knob("add").getValue())
 
-				c.connectInput(1,iMaskInput)
-				c.connectInput(0,iColInput)
+            #mix and mask inputs
+            c.knob("mix").setValue(i.knob("mix").getValue())
+            iMaskInput = i.input(1)
+            iColInput = i.input(0)
 
-				#set position
-				c.knob("xpos").setValue(i.knob("xpos").getValue()+100)
-				c.knob("ypos").setValue(i.knob("ypos").getValue())
+            c.connectInput(1,iMaskInput)
+            c.connectInput(0,iColInput)
 
-			else:
-				print "this only works for grades"
+            #set position
+            c.knob("xpos").setValue(i.knob("xpos").getValue()+100)
+            c.knob("ypos").setValue(i.knob("ypos").getValue())
 
-	gq_gradeConvertWin()
-	
-run()
+        else:
+            print "this only works for grades"
+
+gq_gradeConvertWin()
